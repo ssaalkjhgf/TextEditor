ddf4eb9df84057d03009807e4fe754c2b9958612
钱梦璋 <qmznj1998@163.com>
2017-10-23 10:17:55 +0800

    Initial Commit

---
+++ TextEditor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TextEditor
+//
+//  Created by QMZ on 2017/10/23.
+//  Copyright © 2017年 QMZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
